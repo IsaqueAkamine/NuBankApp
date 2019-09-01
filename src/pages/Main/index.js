708826c47aeb0169fbcb372fd530c0659e8e1724
Isaque Akamine <isaqueakamine@gmail.com>
2019-09-01 14:41:39 -0300

    add tabs container

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 
-import Header from '~/components/Header';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
+
+import { 
+  Container, Content, Card, 
+  CardHeader, CardContent, CardFooter, 
+  Title, Description, Annotation 
+} from './styles';
 
 export default function Main() {
-  return(
+  return (
     <Container>
       <Header />
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 41.235,67</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 50,00 recebida.
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+      <Tabs />
     </Container>
   );
 }
